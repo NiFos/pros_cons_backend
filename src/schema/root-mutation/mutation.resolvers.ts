dca4dd62888426eb8f62be837ec652db0a7614f6
Vlad <vlad.nifos@gmail.com>
2020-09-04 14:33:48 +0300

    Apollo post schema

--- src/schema/root-mutation/mutation.resolvers.ts
+++ src/schema/root-mutation/mutation.resolvers.ts
@@ -1,10 +1,53 @@
+import {
+  updatePost,
+  createPost,
+  addToPost,
+  deletePost,
+} from '../../models/post';
+
 export const RootMutationResolver = {
-  Post: async (
+  UpdatePostTitle: async (
     parent: any,
     args: any,
     context: any,
     info: any
   ): Promise<string> => {
+    const { id, title } = args;
+    const post = await updatePost(id, title);
+    if (post) return id;
+    return '';
+  },
+  CreatePost: async (
+    parent: any,
+    args: any,
+    context: any,
+    info: any
+  ): Promise<string> => {
+    const { title } = args;
+    const post = await createPost(title);
+    if (post.id) return post.id;
+    return '';
+  },
+  AddPostData: async (
+    parent: any,
+    args: any,
+    context: any,
+    info: any
+  ): Promise<string> => {
+    const { id, type, title } = args;
+    const post = await addToPost(id, type, title);
+    if (post) return post;
+    return '';
+  },
+  DeletePost: async (
+    parent: any,
+    args: any,
+    context: any,
+    info: any
+  ): Promise<string> => {
+    const { id } = args;
+    const post = await deletePost(id);
+    if (post) return post;
     return '';
   },
 };
